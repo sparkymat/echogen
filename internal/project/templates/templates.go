7a0beeb2660a086fff3687c88c93ec912eb3000b
Ajith Hussain <ajith.hussain@fastmail.com>
2025-02-13 17:58:07 +1100

    Changed template ext

--- internal/project/templates/templates.go
+++ internal/project/templates/templates.go
@@ -3,36 +3,36 @@ package templates
 import _ "embed"
 
 var (
-	//go:embed main.go.tmpl
+	//go:embed main.go.template
 	MainGo string
 
-	//go:embed go.mod.tmpl
+	//go:embed go.mod.template
 	GoMod string
 
-	//go:embed internal_config_service.go.tmpl
+	//go:embed internal_config_service.go.template
 	ConfigServiceGo string
 
-	//go:embed internal_database_service.go.tmpl
+	//go:embed internal_database_service.go.template
 	DatabaseServiceGo string
 
-	//go:embed internal_route_setup.go.tmpl
+	//go:embed internal_route_setup.go.template
 	RouteSetupGo string
 
-	//go:embed internal_route_web.go.tmpl
+	//go:embed internal_route_web.go.template
 	RouteWebGo string
 
-	//go:embed internal_route_api.go.tmpl
+	//go:embed internal_route_api.go.template
 	RouteAPIGo string
 
-	//go:embed internal_handler_common.go.tmpl
+	//go:embed internal_handler_common.go.template
 	HandlerCommonGo string
 
-	//go:embed internal_handler_register.go.tmpl
+	//go:embed internal_handler_register.go.template
 	HandlerRegisterGo string
 
-	//go:embed internal_handler_login.go.tmpl
+	//go:embed internal_handler_login.go.template
 	HandlerLoginGo string
 
-	//go:embed internal_handler_home.go.tmpl
+	//go:embed internal_handler_home.go.template
 	HandlerHomeGo string
 )
